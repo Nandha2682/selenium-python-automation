df0c27b9f5a5815f2c65c26a7c7ef5133ac052a3
Nandhakumar <nandha2682@gmail.com>
2026-06-21 00:19:54 +0530

    Mini Project 1 - Stage 2 & 3: Add to cart and navigate to cart page

--- pages/Saucedemo/login.py
+++ pages/Saucedemo/login.py
@@ -1,14 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.select import Select
 
 driver = webdriver.Chrome()
 driver.get("https://www.saucedemo.com/")
-driver.find_element(By.ID,"user-name").send_keys("standard_user")
-driver.find_element(By.ID,"password").send_keys("secret_sauce")
-driver.find_element(By.ID,"login-button").click()
+driver.find_element(By.ID, "user-name").send_keys("standard_user")
+driver.find_element(By.ID, "password").send_keys("secret_sauce")
+driver.find_element(By.ID, "login-button").click()
+
+driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
+
+badge_text = driver.find_element(By.CLASS_NAME, "shopping_cart_badge").text
+print("Cart count:", badge_text)
+driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
 print("Current URL:", driver.current_url)
+
 input("Press Enter to close browser...")
 driver.quit()
